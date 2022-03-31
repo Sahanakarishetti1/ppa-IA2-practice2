e7efdc5c3fe5bb4bcccbe801fa24bd412b32a1d3
Sahana karishetti karishetti <96462274+Sahanakarishetti1@users.noreply.github.com>
2022-03-31 13:01:01 +0000

    p3original completed

--- p3original.c
+++ p3original.c
@@ -10,14 +10,19 @@ int is_composite(int n)
 {
   for(int i=2;i<=n;i++)
   {
-    if(n%i==0)
-    return 1;
+      if(n%i==0)
+      {
+        return 0;
+        }
+      else
+      {
+        return 1;
+        }
   }
-  return 0;
-}
+  }
 void output(int n,int is_composite)
 {
-  if(is_composite)
+  if(is_composite==0)
   printf("%d is a composite num\n",n);
   else
   printf("%d is nt a composite  num\n",n);
